54e56b3e91ac83c7184bc9d3b32e14e1c2b233db
363206207x-cmd <363206207x@gmail.com>
2026-05-19 01:37:06 +0800

    feat: add entry completion positive contract dto skeleton

---
+++ src/main/java/org/example/trademodel/dto/planboundary/SourceTraceEntryPositiveCompletionStatusEnum.java
@@ -0,0 +1,7 @@
+package org.example.trademodel.dto.planboundary;
+
+public enum SourceTraceEntryPositiveCompletionStatusEnum {
+    INCOMPLETE,
+    POSITIVE_FIXTURE_READY,
+    POSITIVE_DESIGN_REVIEW_ONLY
+}
